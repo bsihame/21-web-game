80d155ccb42f33f17c9d4f4c763850f3f7095525
bsihame <sihamebazi@pursuit.org>
2019-12-15 12:26:53 -0500

    Add event listner to stay button

--- index.js
+++ index.js
@@ -1,78 +1,82 @@
-document.addEventListener("DOMContentLoaded", async() => {
+document.addEventListener("DOMContentLoaded", async () => {
   //variable
   //getCards()
   // let div = document.querySelector("#startGame");
-  let div = document.querySelector("#startGame")
-  let start = document.querySelector("#start")
+  let div = document.querySelector("#startGame");
+  let start = document.querySelector("#start");
   let deckId;
-  let score = 0
+  let score = 0;
   let count;
   let value;
-  
-  let displayScore = document.createElement("h1")
-  div.appendChild(displayScore);
-  
-  const getId = async () => {
-    try{
-      //get one shuffled deck
-      let card = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
-      deckId = card.data.deck_id
-      debugger
-    } catch(err) {
-      console.log(err)
-    }
-  }
 
-  const getCards = async (num) => {
-    try{
+  let displayScore = document.createElement("h1");
+  div.appendChild(displayScore);
+
+  const getId = async () => {
+    try {
+      //get one shuffled deck
+      let card = await axios.get(
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
+      deckId = card.data.deck_id;
+      debugger;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getCards = async num => {
+    try {
       //fetch shuffle deck
       //let card = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
       //debugger
-      
+
       //deckId = card.data.deck_id
 
-      let drawCards = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${num}`);
-      debugger
-      
-      let cards = drawCards.data.cards
+      let drawCards = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${num}`
+      );
+      debugger;
+
+      let cards = drawCards.data.cards;
       cards.forEach(card => {
         //creat
         let img = document.createElement("img");
         let ulImg = document.createElement("ul");
-        let displayCard = document.createElement("li")
-        
-        
-        img.src = card.image
-        displayCard.appendChild(img)
-        ulImg.appendChild(displayCard)
-        div.appendChild(ulImg)
-        
-        value = card.value
-        if(value === "ACE"){
-          score += 11
-        } else if (value === "KING"||value === "QUEEN"||value === "JACK" ){
-          score += 10
-        } else {
-          score += Number(value)
-        }
-        
-      });
-      
-      displayScore.innerText = score;
-      
-    } catch(err) {
-      console.log(err)
-    }
-  }
-  // add event listener
-  start.addEventListener("click", () => {
-      
-    getCards(2);
-  })
+        let displayCard = document.createElement("li");
 
-  hit.addEventListener("click", ()=> {
+        img.src = card.image;
+        displayCard.appendChild(img);
+        ulImg.appendChild(displayCard);
+        div.appendChild(ulImg);
+
+        value = card.value;
+        if (value === "ACE") {
+          score += 11;
+        } else if (value === "KING" || value === "QUEEN" || value === "JACK") {
+          score += 10;
+        } else {
+          score += Number(value);
+        }
+      });
+
+      displayScore.innerText = score;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // add event listeners
+  start.addEventListener("click", () => {
+    getCards(2);
+  });
+
+  hit.addEventListener("click", () => {
     getCards(1);
   });
-getId()
- 
+
+  stay.addEventListener("click", ()=> {
+    getCards(3);
+  })
+  
+  getId();
 });
